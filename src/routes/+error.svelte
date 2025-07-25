6466f464d7df4411e010cc063f23d1883c609988
Tyler Twining <twijn@twijn.net>
2025-07-25 12:52:24 -0500

    feat: Add simple 404/error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Section from '$lib/components/Section.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import { page } from '$app/state';
+</script>
+
+<svelte:head>
+	<title>{page.status} | Krawlet</title>
+</svelte:head>
+
+<Section lgCols={12}>
+	<h2>{page.status}: {page?.error?.message ?? "Internal Server Error"}</h2>
+	<p>
+		The page responded with a status of {page.status}.
+	</p>
+	<div class="links">
+		<Button variant="primary" href="/" full={true}>
+			Return to Home
+		</Button>
+	</div>
+</Section>
