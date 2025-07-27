42b09436573dbb58eaa17baeb2ca3057d72e97e7
Tyler Twining <twijn@twijn.net>
2025-07-26 19:17:23 -0500

    feat: Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,3 +22,29 @@
 
 	{@render children?.()}
 </div>
+<footer>
+	<p>
+		Made for the <a href="https://reconnected.cc/" target="_blank">Reconnected</a> community
+		by <a href="https://www.twijn.dev" target="_blank">Twijn</a>
+	</p>
+	<p>
+		View the project on <a href="https://github.com/Twijn/krawlet" target="_blank">GitHub</a>
+	</p>
+</footer>
+
+<style>
+	footer {
+			font-size: .8rem;
+			text-align: center;
+      padding: 1rem;
+			opacity: .6;
+	}
+
+	footer p {
+			margin: .25rem 0;
+	}
+
+	footer a {
+			color: var(--text-color-2);
+	}
+</style>
